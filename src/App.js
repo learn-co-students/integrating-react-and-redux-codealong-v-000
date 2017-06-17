152ceae4fb92a87fbdc36886dcecc38a613de8ce
bortch89 <bortch@gmail.com>
2017-06-17 12:12:23 +0200

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+// ./src/App.js
+
 import React, { Component } from 'react';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return (
-      <div className="App" >
-        Counter Component here.
+      <div className="App">
+        <Counter />
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
