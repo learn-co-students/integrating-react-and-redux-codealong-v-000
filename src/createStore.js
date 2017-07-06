c848bbd5188418bad3fdc84adb96bc45618549d4
kkashuda <kkashuda@gmail.com>
2017-07-06 18:34:06 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,33 @@
+import { render } from './index.js';
 
+
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    console.log(`the state is ${state.count}`);
+    console.log(`the action is ${action.type}`);
+    render();
+  };
+
+  function getState(){
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
+
+function changeCount(state = {
+  count: 0,
+}, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+    default:
+      return state;
+  };
+};
