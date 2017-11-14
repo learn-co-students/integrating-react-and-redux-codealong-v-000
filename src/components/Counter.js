90685000726546ea616b627bec00c2ab2672a366
will-sauer-learn <will@heueso.com>
2017-11-14 11:22:08 -0500

    done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+
+//note that this is still a react component (accepts props, returns single element)
+//but it's being defined as an anonymous arrow function
+//and so (I guess) when you import this module, you get the whole thing?
 export default (props) => {
+
+  const handleOnClick = () => {
+    props.store.dispatch({ type: 'INCREASE_COUNT' });
+  }
+
   return (
     <div>
-      Counter Component
+      <button onClick={handleOnClick}>Click Me</button>
+      <div>{props.store.getState().count}</div>
     </div>
   )
 };
