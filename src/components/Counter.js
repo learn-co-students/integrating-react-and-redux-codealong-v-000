fdb8c6e08251da33cdd632d9c84febe9396a63aa
MargaritaKhodzka <mkhodzka@gmail.com>
2017-11-17 21:10:56 -0500

    Done.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
 export default (props) => {
+  //dispatch an action each time the button is clicked
+  const handleOnClick = () => {
+    props.store.dispatch({ type: 'INCREASE_COUNT'})
+  }
+
+  /*
+  add a button and a place to display the current count
+  button has a callback to the onClick event
+  each time a button is clicked it calls the function
+  the function accesses the store from the props
+  dispatches an action to increase the count
+  */
   return (
     <div>
-      Counter Component
+      <button onClick={handleOnClick}>
+        Click Me
+      </button>
+      <div>{props.store.getState().count}</div>
     </div>
   )
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import changeCount from './reducers/changeCount';
+import createStore from './createStore';
+//create the store and pass it through React app as a prop
+//app component has access to the store
+const store = createStore(changeCount);
 
 export function render() {
   ReactDOM.render(
-    <App />,
+    <App store={store} />,
     document.getElementById('root')
   );
 };
 
-render();
+store.dispatch({ type: '@@INIT' })
