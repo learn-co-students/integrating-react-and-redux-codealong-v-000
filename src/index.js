2851639bf6b1a614ac7adec73f9a26daa0ef0127
Chase Paules <cpaules@gmail.com>
2017-11-25 22:16:12 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import changeCount from './reducers/changeCount';
 import createStore from './createStore';
-
+ 
 const store = createStore(changeCount);
-
+ 
 export function render() {
   ReactDOM.render(
     <App store={store} />,
     document.getElementById('root')
   );
 };
-
-render();
+ 
+store.dispatch({ type: '@@INIT' });
