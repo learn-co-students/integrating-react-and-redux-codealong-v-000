7af943b165f20858850749beebf6a53a0dbdf850
Logan McAnsh <logan@mcan.sh>
2017-12-29 14:07:13 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,18 @@
+import { render } from './';
 
+const createStore = reducer => {
+  let state;
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    console.log(`the state is ${state.count}`);
+    console.log(`the action is ${action.type}`);
+    // render()
+  };
+
+  const getState = () => state;
+
+  return { dispatch, getState };
+};
+
+export default createStore;
