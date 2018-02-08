9d34508f10056da4f80532d6e456647592c77b18
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-07 20:30:15 -0800

    redux merged to react

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
 export default (props) => {
+
+  const handleOnClick = () => {
+    props.store.dispatch({ type: 'INCREASE_COUNT'});
+  }
+
   return (
     <div>
-      Counter Component
+      <button onClick={handleOnClick}>
+        Click Me
+      </button>
+      <div>{props.store.getState().count}</div>
     </div>
   )
 };
+
+// SAME THING...READS A BIT BETTER TO ME AT THIS POINT.
+// const Counter = (props) => {
+//   return (
+//     <div>
+//      <button>Click Me</button>
+//      <div>0</div>
+//     </div>
+//   )
+// }
+//
+// export default Counter;
