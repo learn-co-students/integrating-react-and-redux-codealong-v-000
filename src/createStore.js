c88bb9c283f98bc0824a34c0b13d96c38fbabf6a
Soumya Veer <veer.soumya@gmail.com>
2018-02-22 12:14:37 -0800

    integrate React and Redux

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,21 @@
+import { render } from './index.js';
 
+export default function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    console.log(`this state is ${state.count}`);
+    console.log(`this action is ${action.type}`);
+    render()
+  }
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
