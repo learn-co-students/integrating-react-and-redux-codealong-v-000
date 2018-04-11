128a66d935532bb91476b51e7b0228363cdfad24
Autumn Kangas <autumnkangas@gmail.com>
2018-04-11 18:58:52 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,24 @@
+import { render } from './index.js';
+
+export default function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    //dispatch has access to the reducer and state after the store is created,
+    //allowing for subsequent changes to the state when an action is dispatched
+    state = reducer(state, action);
+    console.log(`the state is ${state.count}`);
+    console.log(`the action is ${action.type}`);
+    render();
+  };
+
+  function getState(){
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
 
