7cbf752e818b0281079d64f742182f6a0b3b4bf8
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-05-01 23:57:28 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const store = createStore(changeCount)
+
 export function render() {
   ReactDOM.render(
-    <App />,
+    <App store={store} />,
     document.getElementById('root')
   );
 };
