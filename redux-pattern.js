53f1874d614d62a04dcbc695781357ab59b438f5
remclay <remmy.lauren@gmail.com>
2018-06-21 12:59:12 +1000

    Done.

--- redux-pattern.js
+++ redux-pattern.js
@@ -1,11 +1,11 @@
-function render(){
-  let container = document.getElementById('container');
-  container.innerHTML = store.getState().count;
-}
-
-const store = createStore(changeCount);
-let button = document.getElementById('button');
-
-button.addEventListener('click', function() {
-  store.dispatch({ type: 'INCREASE_COUNT' })
-});
+// function render(){
+//   let container = document.getElementById('container');
+//   container.innerHTML = store.getState().count;
+// }
+//
+// const store = createStore(changeCount);
+// let button = document.getElementById('button');
+//
+// button.addEventListener('click', function() {
+//   store.dispatch({ type: 'INCREASE_COUNT' })
+// });
