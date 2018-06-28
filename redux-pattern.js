73af6b1a5665928504c4f386e2bd9acbb3ccc5dc
NolanHughes <Nhughes987@gmail.com>
2018-06-28 10:15:05 -0500

    Done.

--- redux-pattern.js
+++ redux-pattern.js
@@ -18,12 +18,11 @@ function createStore(reducer) {
   };
 };
 
-function changeCount(state = {
-  count: 0,
-}, action) {
+function changeCount(state = {count: 0}, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
+
     default:
       return state;
   };
