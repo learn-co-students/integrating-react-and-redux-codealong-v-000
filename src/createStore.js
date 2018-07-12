a27487861dd666f48dbf1a71c199fe0cb2753622
Nathaniel Miller <nathaniel@m.ller.io>
2018-07-11 17:58:02 -0700

    Done

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,25 @@
+import { render } from './index.js'
 
+const createStore = reducer => { 
+  let state
+
+  const dispatch = (action) => { 
+    state = reducer(state, action)
+
+    console.log(`the state is ${state.count}`)
+    console.log(`the action is ${action.type}`)
+
+    render()
+  }
+
+  const getState = () => { 
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+}
+
+export default createStore
