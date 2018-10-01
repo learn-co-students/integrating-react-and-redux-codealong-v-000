1ed17d07f92663d5626334877cc995fe18596864
Sebastian <sebgertz@gmail.com>
2018-10-01 10:10:44 -0400

    finished readme, counter works correctly in browser

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,25 @@
 
+// ./src/createStore.js
+
+import { render } from './index.js';
+
+export default function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    console.log(`the state is ${state.count}`)
+    console.log(`the action is ${action.type}`)
+    // store calls on render method to force re-rendering upon change of state
+    render()
+  };
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
